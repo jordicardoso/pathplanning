5e7e67024d97b6f49fcb565023de9c9b6f763a9f
Jordi <jordi.cardoso@gmail.com>
2016-05-10 20:47:51 +0200

    get-hole-coords OK

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cljts "0.2.0"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [cljts "0.2.0"]
+                 [com.vividsolutions/jts "1.11"]]
   :main ^:skip-aot field-pathp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/field_pathp/core.clj
+++ src/field_pathp/core.clj
@@ -1,17 +1,37 @@
 (ns field-pathp.core
   (:refer-clojure :exclude [empty?])
-  (:use [cljts geom analysis transform])
-  (:import [com.vividsolutions.jts.geom
-            Coordinate
-            GeometryFactory
-            PrecisionModel
-            LinearRing
-            Polygon
-            Point]))
+  (:use [cljts geom analysis transform relation io])
+  (:import [com.vividsolutions.jts.geom Coordinate Geometry Polygon LinearRing Point GeometryFactory PrecisionModel]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def pol1 (polygon
+            (linear-ring [(c 7 64) (c 43 64) (c 39 37) (c 7 37) (c 7 64)])
+            [(linear-ring [(c 18 55) (c 33 55) (c 32 51) (c 16 51) (c 18 55)])]))
+
+(defn coord-vec
+  "Convert a JTS Coordinate to a vector."
+  [^Coordinate coordinate]
+  (if (.isNaN (.z coordinate))
+    [(.x coordinate) (.y coordinate)]
+    [(.x coordinate) (.y coordinate) (.z coordinate)]))
+
+(defn get-coords
+  "Return a vec of coords for a JTS Geometry."
+  [^Geometry geometry]
+  (mapv coord-vec (.getCoordinates geometry)))
+
+(defn get-interior-rings
+  "Return a seq of JTS LinearRing that represent the holes in a Polygon."
+  [^Polygon geometry]
+  (for [i (range 0 (.getNumInteriorRing geometry))]
+    (.getInteriorRingN geometry i)))
+
+(defn get-hole-coords
+  "Return a vec of coord data that represents the holes of a JTS Polygon."
+  [^Polygon geometry]
+  (mapv get-coords (get-interior-rings geometry)))
+
+(def hole (get-hole-coords pol1))
+
+(println hole)
 
 
