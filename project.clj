7daf29ecea90dc486bfca039edec0b108833a850
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2016-05-11 15:12:20 +0200

    jts 1.13

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [cljts "0.2.0"]
-                 [com.vividsolutions/jts "1.11"]]
+                 [com.vividsolutions/jts "1.13"]]
   :main ^:skip-aot field-pathp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
