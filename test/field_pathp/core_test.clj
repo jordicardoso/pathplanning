87856b3ad19652f1dbdb5f6a896f4444abbf6c7f
Marc Andreu Fernandez <marcandreuf@gmail.com>
2016-05-17 19:20:25 +0200

    Test cases to cover existing functions

--- test/field_pathp/core_test.clj
+++ test/field_pathp/core_test.clj
@@ -6,10 +6,13 @@
 #! "Documentation about clojure test syntax
 #!   https://clojure.github.io/clojure/clojure.test-api.html"
 
+(def polygonWithoutHoles (polygon (linear-ring [(c 20 40) (c 20 46) (c 34 56) (c 20 40)]) nil))
+
 
 (def squaredPolygonWithSquaredHole (polygon
             (linear-ring [(c 7 64) (c 43 64) (c 39 37) (c 7 37) (c 7 64)])
             [(linear-ring [(c 18 55) (c 33 55) (c 32 51) (c 16 51) (c 18 55)])]))
+
 
 
 
@@ -24,11 +27,29 @@
 (def coord-3d (c 1 2 3))
 
 (deftest Coordinates2DTest
-  (testing "Check coordinate is 2D" (is (true? (isCoordinate2D coord-2d))))
-  (testing "Check coordinate is not 2D" (is (false? (isCoordinate2D coord-3d)))))
+  (testing "Verify coordinate is 2D" (is (true? (isCoordinate2D coord-2d))))
+  (testing "Verify coordinate is not 2D" (is (false? (isCoordinate2D coord-3d)))))
 
 (deftest Coordinates3DTest
-  (testing "Check coordinate is 3D" (is (true? (isCoordinate3D coord-3d))))
-  (testing "Check coordinate is not 3D" (is (false? (isCoordinate3D coord-2d)))))
+  (testing "Verify coordinate is 3D" (is (true? (isCoordinate3D coord-3d))))
+  (testing "Verify coordinate is not 3D" (is (false? (isCoordinate3D coord-2d)))))
+
+(deftest TestGetVectorFromCoordinates
+  (testing "Verify vector values from a given coordinate 2D"
+    (is (= [1.0 2.0] (getVectorFromCoordinate coord-2d))))
+  (testing "Verify vector values from a given coordinate 3D"
+    (is (= [1.0 2.0 3.0] (getVectorFromCoordinate coord-3d))))
+  )
+
+(deftest TestGetCoordinatesFromGeometry
+  (testing "Verify coordinates from a given polygon in 2D Coordinates"
+    (is (= [[20.0 40.0] [20.0 46.0] [34.0 56.0] [20.0 40.0]]
+           (getCoordinatesFromGeometry polygonWithoutHoles))))
+  )
+
+(deftest TestGetCoordinatesOfHole
+  (testing "Verify coordinats of interior hole"
+    (is (= (quote "LINEARRING (18 55, 33 55, 32 51, 16 51, 18 55)")
+           (get-interior-rings squaredPolygonWithSquaredHole)))))
 
 
