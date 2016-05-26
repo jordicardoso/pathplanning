bfca57622459a05dcc1ab0d57c81d570c139b538
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2016-05-26 15:28:51 +0200

    jts 1.13

--- src/field_pathp/core.clj
+++ src/field_pathp/core.clj
@@ -1,11 +1,13 @@
 (ns field-pathp.core
   (:refer-clojure :exclude [empty?])
   (:use [cljts geom analysis transform relation])
-  (:import [com.vividsolutions.jts.geom Coordinate Geometry Polygon LinearRing Point GeometryFactory PrecisionModel]))
+  (:import [com.vividsolutions.jts.geom Coordinate Geometry Polygon LineString LinearRing Point GeometryFactory PrecisionModel]))
 
 (def pol1 (polygon
             (linear-ring [(c 7 64) (c 43 64) (c 39 37) (c 7 37) (c 7 64)])
             [(linear-ring [(c 18 55) (c 33 55) (c 32 51) (c 16 51) (c 18 55)])]))
+
+(def toolsize 5) "Tractor tool size, distance between linestrings"
 
 (defn coord-vec
   "Convert a JTS Coordinate to a vector."
@@ -30,6 +32,20 @@
   [^Polygon geometry]
   (mapv get-coords (get-interior-rings geometry)))
 
+"Variable Definitions needed to decomposition process"
 (def hole (get-hole-coords pol1))
+(def enve (envelope pol1)) "Minimum envolpe rectangle"
+(def cenve (get-coords enve)) "Vertex coords"
+(def xaxis (get (cenve 0) 0)) "X Axis "
+(def yaxistop (get (cenve 1) 1)) "top Y Axis"
+(def yaxisbot (get (cenve 0) 1)) "bottom Y Axis"
+(def envecoord (coordinates enve))
+(def toplns (line-string [(envecoord 1) (envecoord 2)])) "Top linestring of the envelope rectangle"
+(def botlns (line-string [(envecoord 3) (envecoord 4)])) "Bottom linestring of the evelope rectangle"
 
-(println hole)
+"linestrings recursive generator"
+(loop [x xaxis]
+  (when (< x (+ (length toplns) xaxis) )
+    (line-string [(envecoord 1) (envecoord 2)])
+    (println lns)
+    (recur ( + x toolsize))))
