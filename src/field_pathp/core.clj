bdc2c8156ee3648e57e03b0c9632bdb7b14d44be
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2016-06-13 18:12:25 +0200

    Linestrings for polygon decomposition

--- src/field_pathp/core.clj
+++ src/field_pathp/core.clj
@@ -1,9 +1,21 @@
 (ns field-pathp.core
   (:refer-clojure :exclude [empty?])
   (:use [cljts geom analysis transform relation])
-  (:import [com.vividsolutions.jts.geom Coordinate Geometry Polygon LineString LinearRing Point GeometryFactory PrecisionModel]))
+  (:import [com.vividsolutions.jts.geom Coordinate Geometry Polygon LineString LinearRing Point GeometryFactory PrecisionModel])
+  (:import [com.vividsolutions.jts.operation.overlay.snap SnapOverlayOp]))
 
 (defonce ^:private ^GeometryFactory geom-factory (GeometryFactory.))
+
+"Protocol definitions"
+(defprotocol SimplicityTest
+  (psimple? [geometry]))
+
+(defprotocol SpatialAnalysis
+  (decompose [this that] "Decomposition between 2 geometries"))
+
+(extend-type SnapOverlayOp
+  SpatialAnalysis
+  (decompose [this other] (.SnapOverlayOp this other)))
 
 (def pol1 (polygon
             (linear-ring [(c 7 64) (c 43 64) (c 39 37) (c 7 37) (c 7 64)])
@@ -34,6 +46,9 @@
   [^Polygon geometry]
   (mapv get-coords (get-interior-rings geometry)))
 
+
+
+
 "Variable Definitions needed to decomposition process"
 (def hole (get-hole-coords pol1)) "Coordenades dels forats"
 (def enve (envelope pol1)) "Minimum envolpe rectangle"
@@ -53,5 +68,7 @@
 (def minx (apply min-key first (hole 0)))
 (def maxx (apply max-key first (hole 0)))
 
-(def decompln1 (line-string [(c (maxx 0) yaxistop) (c (maxx 0) yaxisbot)]))
+(def decompln1 (line-string [(c (minx 0) yaxistop) (c (minx 0) yaxisbot)]))
 (def decompln2 (line-string [(c (maxx 0) yaxistop) (c (maxx 0) yaxisbot)]))
+
+(def geomdec (.SnapOverlayOp pol1 decompln1))
